4b3164db73a2c29a78e9c31193ca708318083268
lukaszszura <szura.lukasz@gmail.com>
2022-12-16 11:05:15 +0000

    First commit

---
+++ recipes/setup.rb
@@ -0,0 +1,20 @@
+package 'vim-enhanced'
+package 'nano'
+
+package 'tree' do 
+    action :install
+end
+
+package 'git' do
+    action :install
+end
+
+package 'ntp'
+
+file 'etc/motd' do
+    content 'this server is mine'
+    action :create
+    owner 'root'
+    group 'root'
+end
+
